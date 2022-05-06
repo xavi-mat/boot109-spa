b08f8d75bcd6766558c013563d8fa4cd40202646
alextebbitt <alex.tebbitt87@gmail.com>
2022-05-06 13:52:25 +0200

    comments and removed back button function

--- main.js
+++ main.js
@@ -65,8 +65,8 @@ function goHome() {
     // }
 
     if (database.length > 0) {
-        statsBox.innerHTML = '';
-        showChart(database);
+        statsBox.innerHTML = '';  // if some1 has done test at least once put empty string 
+        showChart(database);      // then show on empty array results from chart on empty string
     } else {
         statsBox.innerHTML = 'There are no stats to show.';
     }
@@ -130,18 +130,18 @@ function startQuiz() {
 }
 
 function hideAllSections() {
-    spinnerBox.classList.remove('d-none');
+    spinnerBox.classList.remove('d-none'); // shows spinner
     document
         .querySelectorAll("section")
-        .forEach((sect) => sect.classList.add("d-none"));
+        .forEach((sect) => sect.classList.add("d-none")); // removes all sections
 
 }
 
 function showSection(section) {
-    // We love to see the spinner!!
+    // We love to see the spinner!! for one second 
     setTimeout(() => {
-        section.classList.remove('d-none');
-        spinnerBox.classList.add('d-none');
+        section.classList.remove('d-none'); // shows sections
+        spinnerBox.classList.add('d-none'); // removes spinner
     }, 1000);
 }
 
@@ -153,14 +153,15 @@ function getQuestions() {
     //         showNextQuestion();  // perform function
     // });
 
-    fetch(APIURL)
-        .then((resp) => resp.json())
-        .then((data) => data.results)
-        .then(dataResults => {
-            questions = dataResults;
+    fetch(APIURL)  // gets the data of quiz from the api
+        .then((resp) => resp.json()) // then waits for data to arrive, converts to an object whatever comes from json is put into the next parameter of .then
+        .then((data) => data.results) // we chose data. data is the object with all info from api. result is the property of the info we have recieved from api
+        // taking data as an object returns data.results. Returns only the thing we want, which is resutls not reponse code.
+        .then(dataResults => {  // 
+            questions = dataResults; // now questions is now an array with all the questions
         })
-        .then(() => {
-            showNextQuestion();
+        .then(() => {   // wait for questions to have questions then show next quesiton function 
+            showNextQuestion(); // 
         })
         .catch(()=>{
             goHome();
@@ -170,30 +171,33 @@ function getQuestions() {
 
 function showNextQuestion() {
     // get question item
-    const questionItem = questions[currentQuestionIndex];
+    const questionItem = questions[currentQuestionIndex];//1 element of array is taken from questions
     // put the question text
-    questionText.innerHTML = questionItem.question;
+    questionText.innerHTML = questionItem.question; // question text is an element from dom(div) 
+    //pintar
+    
     // empty the answers box
-    while (answersBox.firstChild) {
-        answersBox.removeChild(answersBox.firstChild);
+    while (answersBox.firstChild) {  
+      answersBox.removeChild(answersBox.firstChild);
     }
+
     // put the four answers
-    const answers = [...questionItem.incorrect_answers];
-    answers.push(questionItem.correct_answer);
+    const answers = [...questionItem.incorrect_answers]; //  with ... making a copy of the inncorect ansswrs array
+    answers.push(questionItem.correct_answer);  //add correct answer to array of inncorect answer 
     shuffleArray(answers);
 
-    answers.forEach((answer) => {
-        const button = document.createElement("button");
-        button.innerText = answer;
-        button.classList.add("btn", "btn-primary", "m-1");
+    answers.forEach((answer) => {   // iterates over answerrs
+        const button = document.createElement("button"); // creating new button and declaring it
+        button.innerHTML = answer; // paints the four answers in the button
+        button.classList.add("btn", "btn-primary", "m-1"); // makes button which are the answersblu????
         if (answer === questionItem.correct_answer) {
-            button.dataset.correct = true;
+            button.dataset.correct = true; //dataset is a list of attributes, 
         }
         button.addEventListener("click", selectAnswer);
-        answersBox.appendChild(button);
+        answersBox.appendChild(button); ///  add four question buttons to div
     });
     // activate the next button
-    if (currentQuestionIndex === questions.length - 1) {
+    if (currentQuestionIndex === questions.length - 1) {  // if user is at last question
         nextBtn.innerText = "See results";
     } else {
         nextBtn.innerText = "Next";
@@ -212,35 +216,35 @@ function selectAnswer(ev) {
     const clickedButton = ev.target;
 
     clickedButton.classList.remove("btn-primary");  // Remove blue color
-    recolorClickedButton(clickedButton);
-    nextBtn.disabled = false;
+    recolorClickedButton(clickedButton); 
+    nextBtn.disabled = false;     // makes next button available after selecting an answer
     disableAllAnswerButtons();
     updateProgressBar();
 }
 
 function recolorClickedButton(clickedButton) {
 
-    if (clickedButton.dataset.correct) {
+    if (clickedButton.dataset.correct) { // if button you clicked is the correct turn to green
         clickedButton.classList.add("btn-success");
-        points.right++;
+        points.right++;                       //  add one to correct answers
         showToast('success', 'Correct!');
 
     } else {
-        clickedButton.classList.add("btn-danger");
-        points.wrong++;
+        clickedButton.classList.add("btn-danger"); // else turn red
+        points.wrong++;                            // add 1 to wrong answers
         showToast('danger', 'Incorrect!');
     }
 }
 
 
-function disableAllAnswerButtons() {
+function disableAllAnswerButtons() {   // after selecting an answer, it disables all other answers
     Array.from(answersBox.children).forEach((button) => {
         button.disabled = true;
     });
 }
 function goNextQuestion() {
 
-    if (currentQuestionIndex < questions.length) {
+    if (currentQuestionIndex < questions.length) { 
         // There are more questions:
         showNextQuestion();
 
@@ -259,14 +263,14 @@ function getSavedData() {
 }
 
 function saveResults() {
-    const database = getSavedData();
+    const database = getSavedData();  //the result of a function saved in a variable
     const resultsData = {
-        date: new Date().toLocaleString('es'),
-        correctAnswers: points.right,
-        incorrectAnswers: points.wrong,
+        date: new Date().toLocaleString('es'), // the date
+        correctAnswers: points.right,          // correct answers
+        incorrectAnswers: points.wrong,        // incorrect answers
     };
     database.push(resultsData); //pushing infoOfUsers to database array
-    localStorage.setItem("results", JSON.stringify(database));
+    localStorage.setItem("results", JSON.stringify(database)); // puts data back into local storage
 }
 
 function showResults() {
@@ -275,29 +279,23 @@ function showResults() {
     //     `Well done, you scored ${points.right} correct answers and ${points.wrong} wrong answers.`
     //   );
 
-    gradeBox.innerHTML = `${points.right} / ${points.right + points.wrong}`;
-
+    gradeBox.innerHTML = `${points.right} / ${points.right + points.wrong}`; // paints right
+    // and wrong answers into gradebox section
     if (points.right >= 8) {
         resultMsg.innerHTML = `Well done, you scored ${points.right} correct answers and ${points.wrong} wrong answers.`;
     } else if (points.right >= 4) {
         resultMsg.innerHTML = `Not bad. you scored ${points.right} correct answers and ${points.wrong} wrong answers.`;
     } else {
-        resultMsg.innerHTML = `Unfortunatelly, you scored ${points.right} correct answer(s) and ${points.wrong} wrong answer(s).`;
+        resultMsg.innerHTML = `Unfortunately, you scored ${points.right} correct answer(s) and ${points.wrong} wrong answer(s).`;
     }
     showSection(resultsSect);
 }
 
-function goBackQuestion() {
-    if (currentQuestionIndex > 1) {
-        currentQuestionIndex -= 2; // shorthand for minus 2
-        showNextQuestion();
-    }
-}
 
 function updateProgressBar() {
-    let progress = 100 * currentQuestionIndex / NUM_QUESTIONS;
-    progress = Math.floor(progress);
-    progressBar.style.width = `${progress}%`;
+    let progress = 100 * currentQuestionIndex / NUM_QUESTIONS; // find percentage of progress
+    progress = Math.floor(progress);   /// get rid of decimal points
+    progressBar.style.width = `${progress}%`;  // show progress bar progress
 }
 
 function showToast(color, text) {
@@ -363,4 +361,5 @@ homeBtnResults.addEventListener("click", goHome);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Init
-goHome();
+goHome(); // we need this here because if not if wouldnt go back to home page
+// it would just ask us if we wanted to take quiz again
